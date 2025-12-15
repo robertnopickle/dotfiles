c772095b8315b508a6ce3f74cd7d0f24f9f28658
Robert Garmhausen <robertnopickle@github.com>
2025-12-15 16:07:31 -0500

    add some setup stuff

---
+++ setup.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+exec > >(tee -i $HOME/dotfiles_install_log)
+exec 2>&1
+set -x
+
+# Install Packages
+sudo apt-get update -y
+sudo apt-get --assume-yes install fuse ripgrep universal-ctags
+
+# Install Neovim
+sudo modprobe fuse
+sudo groupadd fuse
+sudo usermod -a -G fuse "$(whoami)"
+wget https://github.com/neovim/neovim-releases/releases/download/v0.11.4/nvim-linux-x86_64.appimage
+sudo chmod u+x nvim-linux-x86_64.appimage
+sudo mv nvim-linux-x86_64.appimage /usr/local/bin/nvim
+
+# Install tmux
+wget https://github.com/nelsonenzo/tmux-appimage/releases/download/3.2a/tmux.appimage
+sudo chmod u+x tmux.appimage
+sudo mv tmux.appimage /usr/local/bin/tmux
+
+# Copy zsh config
+rm -f $HOME/.zshrc
+ln -sf $(pwd)/.zshrc $HOME/.zshrc
+
+# Install node + npm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
+\. "$HOME/.nvm/nvm.sh"
+nvm install 24
+
+# Install Copilot CLI
+npm install -g @github/copilot
+
+# Dotfiles..
+mkdir -p $HOME/.config/nvim
+
+ln -sf $(pwd)/.tmux.conf $HOME/.tmux.conf
+
+ln -sf $(pwd)/.config/nvim/* $HOME/.config/nvim/
+ln -sf $(pwd)/.gitconfig $HOME/.gitconfig
+ln -sf $(pwd)/.ignore $HOME/.ignore
+
+# Install NeoVim plugins
+nvim --headless "+Lazy! sync" +qa
+
+sudo chsh -s "$(which zsh)" "$(whoami)"u
