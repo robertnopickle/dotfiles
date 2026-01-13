2e77bc98212f1043907fb31207feb4bf96f86fb1
Robert Garmhausen <robertnopickle@github.com>
2026-01-12 20:30:51 -0500

    triaging a kickstart issue...

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -683,12 +683,12 @@ require('lazy').setup({
         -- But for many setups, the LSP (`ts_ls`) will work just fine
         -- ts_ls = {},
         --
-        bashls = {},
+        -- bashls = {},
         ruby_lsp = {},
         copilot = {},
         eslint = {},
-        cssmodules_ls = {},
-        graphql = {},
+        -- cssmodules_ls = {},
+        -- graphql = {},
         -- gopls = {},
 
         lua_ls = {
